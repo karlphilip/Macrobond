8e8f29b39bd9aec39c6729e67759e89986008bcc
Karl Philip Nilsson <71814828+karlphilip@users.noreply.github.com>
2021-10-14 10:54:50 +0200

    Update upload_series_with_revisions.r

--- R/upload_series_with_revisions.r
+++ R/upload_series_with_revisions.r
@@ -17,18 +17,18 @@ today<-as.Date(Sys.time())
 # Metadata for upload
 # Name (Series identifier)
 account<-"ih:mb:priv:" # use "ih:mb:priv:" for personal account, "ih:mb:dept:" for department account, "ih:mb:com:" for company account
-name<-"random" # the series identifier, only lowercases and numbers (must start with a letter)
+name<-"series" # the series identifier, only lowercases and numbers (must start with a letter)
 fullnamewithdate<-paste(account, name, today, sep="") # Combining the full name with the date
 fullnamewithdate<-str_replace_all(fullnamewithdate, "-", "") # Replacing the "-", since we can't use symbols
 
 # Series description (Display name in the application)
-description<-"My random series" # The series name
+description<-"My series" # The series name
 decriptionwithdate<-paste(description, today) # Combining the description with date today (which is the date of upload) 
 
 # Region
 region<-"us" # Specifying the region name, see the list of regions here: https://techinfo.macrobond.com/regions-list/
 
-category<-"Random" # Specifying the category of the series
+category<-"Series with revision" # Specifying the category of the series
 
 frequency<-"Monthly" # Specifying the series frequency
 
